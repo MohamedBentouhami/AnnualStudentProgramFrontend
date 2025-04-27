c7e63fc63b86630efd2ea9560eabdc705a9bdaba
Mohamed <mohamedbentouhami512@gmail.com>
2025-04-27 12:09:45 +0200

    add some style to the navbar

--- src/components/nav-bar/nav-bar.jsx
+++ src/components/nav-bar/nav-bar.jsx
@@ -1,9 +1,13 @@
 import { NavLink } from "react-router-dom";
 
+import "./nav-bar.css"
+import { ChalkboardSimple, House, Student } from "@phosphor-icons/react";
+
 export default function NavBar() {
-    return <ul>
-        <li> <NavLink to="/">Home</NavLink></li>
-        <li> <NavLink to="/courses">Courses</NavLink></li>
-        <li> <NavLink to="/students">Students</NavLink></li>
+    return <ul className="nav-bar">
+        <li> <NavLink to="/">
+            <House />Home</NavLink></li>
+        <li> <NavLink to="/courses"><ChalkboardSimple />Courses</NavLink></li>
+        <li> <NavLink to="/students"> <Student />Students</NavLink></li>
     </ul>
 }
--- src/containers/header/header.jsx
+++ src/containers/header/header.jsx
@@ -1,5 +1,9 @@
 import NavBar from "../../components/nav-bar/nav-bar.jsx";
-
+import logo  from "../../assets/logo.png"
+import "./header.css"
 export default function Header() {
-    return <header><NavBar /></header>
+    return <header className="header">
+        <img src={logo}></img>
+        <NavBar />
+    </header>
 }
