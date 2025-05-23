f686df5f9639a3e74cc32908cc79f147a48ab91a
Mohamed <mohamedbentouhami512@gmail.com>
2025-05-23 17:02:24 +0200

    add seach students bar

--- src/containers/display-students/display-students.jsx
+++ src/containers/display-students/display-students.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux"
 import { studentsFetch } from "../../store/students/student.action.js";
 import Loader from "../../components/loader/loader.jsx";
@@ -8,12 +8,24 @@ import StudentsTable from "../../components/students-table/students-table.jsx";
 export default function DisplayStudents() {
     const dispatch = useDispatch();
     const { isLoading, students, error, hasBeenFetched } = useSelector((state) => state.student);
-
+    const [filteredStudents, setFilteredStudents] = useState([]);
     useEffect(() => {
         if (!hasBeenFetched) {
             dispatch(studentsFetch());
         }
     }, [hasBeenFetched])
+
+    useEffect(() => {
+        setFilteredStudents(students || []);
+    }, [students]);
+
+    const handleSearch = (e) => {
+        const filter = e.target.value.toLowerCase();
+        if (!students) return;
+        const filtered = students.filter((student) => student.name.toLowerCase().startsWith(filter));
+        console.log(filtered)
+        setFilteredStudents(filtered)
+    }
 
     if (error) return (<div><p className="msg">Failed to load students</p></div>)
     if (isLoading) return <Loader />
@@ -21,7 +33,11 @@ export default function DisplayStudents() {
 
     return <div className="student-container">
         <h1 className="student-title">List of students</h1>
-        <StudentsTable students={students} ></StudentsTable>
+        <div className="search-container">
+            <label htmlFor="student-search">Search by name: </label>
+            <input id="student-search" type="search" onChange={handleSearch}></input>
+        </div>
+        <StudentsTable students={filteredStudents} ></StudentsTable>
     </div>
 
 }
